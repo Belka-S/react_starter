dc17ec7090a32e79b168a6d6a0a82a2f13a16240
Belka-S <belka.srg@gmail.com>
2023-04-20 07:52:27 +0200

    fix section

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -2,8 +2,8 @@ import { Container } from './Section.styled';
 
 export const Section = ({ mainTitle, title, children }) => (
   <Container>
-    <h1>{mainTitle}</h1>
-    <h2>{title}</h2>
+    {mainTitle && <h1>{mainTitle}</h1>}
+    {title && <h2>{title}</h2>}
     {children}
   </Container>
 );
